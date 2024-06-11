ab877fe4fb1a20ebcf03ed6031dbe71ec68d8615
Ansgar666 <oscardct12@gmail.com>
2024-06-11 14:30:58 +0200

    Update script.js

--- IA/proyecto-curso-AI-master/proyecto-curso-AI-master/js/script.js
+++ IA/proyecto-curso-AI-master/proyecto-curso-AI-master/js/script.js
@@ -10,6 +10,7 @@ function preload (){
     img6 = loadImage (`assets/goat.jpg`)  
     audio = createAudio (`assets/Marduk.mp3`)
     audio2 = createAudio (`assets/FreezingMoon.mp3`)
+    audio3 = createAudio (`assets/riff.mp3`)
 }
 function setup() {
     //Este codigo se ejecuta 1 vez solamente, al principio
@@ -18,7 +19,7 @@ function setup() {
     
     var x = 0;
     var y = 0;
-   audio.play();
+   audio2.play();
     for(let j = 0; j<3;j++){
         for (let index = 0; index < 10; index++) {
         rect(x,y,128,50)
@@ -49,8 +50,10 @@ function comprobarSiChocaConLadrillos(){
                     y:ladrillo.y,
                     w:20,
                     h:20
+            
                 }
                 bola2.push(power);
+                audio3.play();
             }
                 
             }    
@@ -65,9 +68,12 @@ var posYBola = 675;
 var vxPala = 10;
 var vxBola = 5;
 var vyBola = -5;
-var vyBola2 = 2;
+var vyBola2 = 4;
 var posYBola2 = 0;
 var posXBola2 = 128;
+var wPala = 90;
+var hPala = 40;
+var zPala = 20;
 function draw() {
     //Este codigo se ejecuta cada FRAME.
     background(img);
@@ -75,14 +81,14 @@ function draw() {
     comprobarSiChocaConLadrillos();
     
     image(img4,posXBola, posYBola, 20, 20)
-    image(img2,posXPala,posYPala, 90, 40, 20); 
+    image(img2,posXPala,posYPala, wPala, hPala, zPala); 
     for (let i=0; i<bola2.length; i++){
         var power = bola2[i];
         image(img6,power.x, power.y, 40, 40);
         power.y = power.y + vyBola2
         power.x = power.x
         if(power.y == posYPala && (power.x >=posXPala && power.x <= posXPala+90)){
-            vxPala = vxPala * 2;  
+            wPala = wPala + 50; 
               
          }
     }
@@ -96,12 +102,12 @@ function draw() {
    
     if(posYBola ==720){
         image (img5,0,0,1280, 720);
-        frameRate(0)
-      audio.stop();
-      audio2.play();
+        
+      audio2.stop();
+      audio.play();
         alert ("Presiona espacio para reiniciar")
     }
-    if(posYBola == posYPala && (posXBola >=posXPala && posXBola <= posXPala+90)){
+    if(posYBola == posYPala && (posXBola >=posXPala && posXBola <= posXPala+wPala)){
         vyBola = vyBola * -1;
     }
     
@@ -115,7 +121,7 @@ function draw() {
 function keydown() {
     if (keyIsDown (37) && posXPala > 0) {
       posXPala=posXPala-vxPala;
-    } else if (keyIsDown (39) && posXPala < 1190) {
+    } else if (keyIsDown (39) && posXPala < (1280-wPala)) {
       posXPala=posXPala+vxPala;
     }
     if (keyIsDown (32) ){
