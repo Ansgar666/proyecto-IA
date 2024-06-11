7a49c889a9bce4329a5efab0f341b16855febe34
Ansgar666 <oscardct12@gmail.com>
2024-06-11 13:08:41 +0200

    Update script.js

--- IA/proyecto-curso-AI-master/proyecto-curso-AI-master/js/script.js
+++ IA/proyecto-curso-AI-master/proyecto-curso-AI-master/js/script.js
@@ -3,10 +3,13 @@ var ladrillos = [];
 var bola2 = [];
 function preload (){
     img = loadImage (`assets/bosque.avif`)
-    img2 = loadImage (`assets/espada.png`)
-    img3 = loadImage (`assets/ladrillo.png`)
-    img4 = loadImage (`assets/pentagrama.png`)
+    img2 = loadImage (`assets/espada2.png`)
+    img3 = loadImage (`assets/ladrillos.webp`)
+    img4 = loadImage (`assets/pentagrama3.png`)
     img5 = loadImage(`assets/died.webp`)
+    img6 = loadImage (`assets/goat.jpg`)  
+    audio = createAudio (`assets/Marduk.mp3`)
+    audio2 = createAudio (`assets/FreezingMoon.mp3`)
 }
 function setup() {
     //Este codigo se ejecuta 1 vez solamente, al principio
@@ -15,7 +18,7 @@ function setup() {
     
     var x = 0;
     var y = 0;
-   
+   audio.play();
     for(let j = 0; j<3;j++){
         for (let index = 0; index < 10; index++) {
         rect(x,y,128,50)
@@ -41,7 +44,7 @@ function comprobarSiChocaConLadrillos(){
             if ((ladrillo.y+50 == posYBola && posXBola >= ladrillo.x) && posXBola <= ladrillo.x +128 ){
                 ladrillo.isBroken = true;
                 vyBola = vyBola *-1;
-                let power = {
+                if(Math.random() > 0.5){let power = {
                     x:ladrillo.x + 64,
                     y:ladrillo.y,
                     w:20,
@@ -49,13 +52,14 @@ function comprobarSiChocaConLadrillos(){
                 }
                 bola2.push(power);
             }
-            
+                
+            }    
      }
     }   )
 }
 
 var posXPala = 610;
-var posYPala = 690
+var posYPala = 680
 var posXBola = 618;
 var posYBola = 675;
 var vxPala = 10;
@@ -70,13 +74,17 @@ function draw() {
     redibujarLadrillos ();
     comprobarSiChocaConLadrillos();
     
-    image(img4,posXBola, posYBola, 15, 15)
+    image(img4,posXBola, posYBola, 20, 20)
     image(img2,posXPala,posYPala, 90, 40, 20); 
     for (let i=0; i<bola2.length; i++){
         var power = bola2[i];
-        ellipse(power.x, power.y, 20, 20);
+        image(img6,power.x, power.y, 40, 40);
         power.y = power.y + vyBola2
         power.x = power.x
+        if(power.y == posYPala && (power.x >=posXPala && power.x <= posXPala+90)){
+            vxPala = vxPala * 2;  
+              
+         }
     }
     
      if(posXBola<=0 || posXBola >=1280){
@@ -88,15 +96,15 @@ function draw() {
    
     if(posYBola ==720){
         image (img5,0,0,1280, 720);
-        frameRate(0);
+        frameRate(0)
+      audio.stop();
+      audio2.play();
         alert ("Presiona espacio para reiniciar")
     }
     if(posYBola == posYPala && (posXBola >=posXPala && posXBola <= posXPala+90)){
         vyBola = vyBola * -1;
     }
-    if(posYBola2 == posYPala && (posXBola2 >=posXPala && posXBola2 <= posXPala+90)){
-       vxPala = vxPala * 2;    
-    }
+    
     posXBola = posXBola + vxBola
     posYBola = posYBola + vyBola
  
@@ -110,7 +118,7 @@ function keydown() {
     } else if (keyIsDown (39) && posXPala < 1190) {
       posXPala=posXPala+vxPala;
     }
-    if (keyIsDown (32) && frameRate (0)){
+    if (keyIsDown (32) ){
        
         location.reload();
     
